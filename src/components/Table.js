2509339b3a4464cdd79b09cf51bd9ffc6092ac1b
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-11-01 15:28:56 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <caption>Tabela de gastos:</caption>
+          <tr>
+            <th scope="col">Descrição</th>
+            <th scope="col">Tag</th>
+            <th scope="col">Método de pagamento</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Moeda</th>
+            <th scope="col">Câmbio utilizado</th>
+            <th scope="col">Valor convertido</th>
+            <th scope="col">Moeda de conversão</th>
+            <th scope="col">Editar/Excluir</th>
+          </tr>
+          {expenses.map((ele, index) => (
+            <tr key={ index }>
+              <td>{ele.description}</td>
+              <td>{ele.tag}</td>
+              <td>{ele.method}</td>
+              <td>{ele.value}</td>
+              <td>{ele.currency}</td>
+              {Number(ele.exchangeRates[ele.currency].ask).toFixed(2)}
+              <td>
+                {(Number(ele.exchangeRates[ele.currency]
+                  .ask) * ele.value).toFixed(2)}
+
+              </td>
+              <td>BRL</td>
+              <td><button type="button">X</button></td>
+            </tr>
+          ))}
+
+        </table>
+
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (globalState) => ({
+  expenses: globalState.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf.isRequired,
+
+};
+
+export default connect(mapStateToProps)(Table);
