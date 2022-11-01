8513b05b65abe093fb2648a78b068e81a483d084
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-11-01 15:43:10 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,34 +10,35 @@ class Table extends Component {
         <table>
           <caption>Tabela de gastos:</caption>
           <tr>
-            <th scope="col">Descrição</th>
-            <th scope="col">Tag</th>
-            <th scope="col">Método de pagamento</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Moeda</th>
-            <th scope="col">Câmbio utilizado</th>
-            <th scope="col">Valor convertido</th>
-            <th scope="col">Moeda de conversão</th>
-            <th scope="col">Editar/Excluir</th>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
-          {expenses.map((ele, index) => (
-            <tr key={ index }>
-              <td>{ele.description}</td>
-              <td>{ele.tag}</td>
-              <td>{ele.method}</td>
-              <td>{ele.value}</td>
-              <td>{ele.currency}</td>
-              {Number(ele.exchangeRates[ele.currency].ask).toFixed(2)}
-              <td>
-                {(Number(ele.exchangeRates[ele.currency]
-                  .ask) * ele.value).toFixed(2)}
+          <tbody>
+            {expenses.map((ele) => (
+              <tr key={ ele.id }>
+                <td>{ele.description}</td>
+                <td>{ele.tag}</td>
+                <td>{ele.method}</td>
+                <td>{ele.value}</td>
+                <td>{ele.exchangeRates[ele.currency].name}</td>
+                {Number(ele.exchangeRates[ele.currency].ask).toFixed(2)}
+                <td>
+                  {(Number(ele.exchangeRates[ele.currency]
+                    .ask) * ele.value).toFixed(2)}
 
-              </td>
-              <td>BRL</td>
-              <td><button type="button">X</button></td>
-            </tr>
-          ))}
-
+                </td>
+                <td>BRL</td>
+                <td><button type="button">X</button></td>
+              </tr>
+            ))}
+          </tbody>
         </table>
 
       </div>
