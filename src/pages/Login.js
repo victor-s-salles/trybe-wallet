0e67e123082a459f0b8b8e60feec330513bdb0d1
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-11-02 16:55:14 -0300

    CSS da pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { login } from '../redux/actions';
+import '../CSS/Login.css';
+import logoLogin from '../imgs/logoTrybeWallet.png';
 
 class Login extends React.Component {
   constructor() {
@@ -42,39 +44,45 @@ class Login extends React.Component {
   render() {
     const { email, password, isDisable } = this.state;
     return (
-      <div>
-        <label htmlFor="email">
-          Email:
-          <input
-            data-testid="email-input"
-            id="email"
-            name="email"
-            type="text"
-            value={ email }
-            onChange={ this.handleChange }
-            placeholder="Email"
-          />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            data-testid="password-input"
-            id="password"
-            name="password"
-            type="text"
-            value={ password }
-            onChange={ this.handleChange }
-            placeholder="Senha"
-          />
-        </label>
-        <button
-          type="button"
-          disabled={ isDisable }
-          onClick={ this.loginBTN }
-        >
-          Entrar
+      <div className="Login-PrincipalDiv">
+        <div className="Login-Second-Div">
+          <img src={ logoLogin } alt="Logo TrybeWallet" />
+          <div className="Login-InputDiv">
+            <label htmlFor="email">
+              {/* Email: */}
+              <input
+                data-testid="email-input"
+                id="email"
+                name="email"
+                type="text"
+                value={ email }
+                onChange={ this.handleChange }
+                placeholder="Email"
+              />
+            </label>
+            <label htmlFor="password">
+              {/* Senha: */}
+              <input
+                data-testid="password-input"
+                id="password"
+                name="password"
+                type="text"
+                value={ password }
+                onChange={ this.handleChange }
+                placeholder="Senha"
+              />
+            </label>
 
-        </button>
+            <button
+              type="button"
+              disabled={ isDisable }
+              onClick={ this.loginBTN }
+            >
+              Entrar
+
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
